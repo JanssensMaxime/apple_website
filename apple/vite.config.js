ae3a1b9b3f0255dc39d1e3d7639e86e4b3b64845
Maxime Janssens <janssensmaxime31@gmail.com>
2025-04-09 10:39:20 +0200

    apple_website-done

---
+++ apple/vite.config.js
@@ -0,0 +1,12 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  build: {
+    sourcemap: true,
+  },
+});
